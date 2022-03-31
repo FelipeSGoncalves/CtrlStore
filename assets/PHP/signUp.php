fa4bd82051e162a6b12d8aaa8aa801976e5a6211
LeinadRPFI <daniel.infoifpr@gmail.com>
2022-03-31 13:03:05 -0300

    Update
    Cadastro de cliente funcionando.

--- assets/PHP/signUp.php
+++ assets/PHP/signUp.php
@@ -1,26 +1,24 @@
 <?php
 
-    echo 'OI GATA';
-
     include_once("conexao.php");
 
-    $name = $_POST['nome'];
+    $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $phone = $_POST['phone'];
     $cpf = $_POST['cpf'];
     $birthdate = $_POST['birthdate'];
 
-    echo 'OI GATA 2';
-
-
-    if (isset($nome) == "" or isset($email) == "" or isset($senha) == "" or isset($telefone)  == "" or isset($cpf) == "" or isset($dataNascimento) == ""){
-        "Voce não preencheu todos os dados";
-    } else {
-        $result_usuario = "INSERT INTO tbClient(client__name, client__email, client__password, client__phone, client__cpf, client__birthdate) 
+    if (isset($name) == "" or isset($email) == "" or isset($password) == "" or isset($phone)  == "" or isset($cpf) == "" or isset($birthdate) == ""){
+        echo "Voce não preencheu todos os dados";
+    } 
+    
+    else {
+        $result_usuario = "INSERT INTO tbclient(client__name, client__email, client__password, client__phone, client__cpf, client__birthdate) 
                             VALUES ('$name', '$email', '$password', '$phone', '$cpf', '$birthdate')";
         $result_usuario = mysqli_query($conexao, $result_usuario);
         echo "Cadastrado com sucesso";
     }
+
 
 ?>
